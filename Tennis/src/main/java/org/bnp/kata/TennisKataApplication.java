44580d26f78306b1330d9298bab2fb6d5c53eb70
Ankit Kanwar <ankit@Ankits-MacBook-Air.local>
2021-01-03 18:31:40 +0530

    feat:create spring boot application

---
+++ Tennis/src/main/java/org/bnp/kata/TennisKataApplication.java
@@ -0,0 +1,15 @@
+package org.bnp.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisKataApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TennisKataApplication.class, args);
+	}
+	
+}
+
+
